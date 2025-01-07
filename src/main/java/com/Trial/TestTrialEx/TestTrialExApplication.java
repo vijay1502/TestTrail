5783e4b4b6d290cbdc63f2d155ff6586c2e761cc
vijay1502 <pruthvikrishna97@gmail.com>
2025-01-07 11:28:02 +0530

    Initial Commit

---
+++ src/main/java/com/Trial/TestTrialEx/TestTrialExApplication.java
@@ -0,0 +1,13 @@
+package com.Trial.TestTrialEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTrialExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTrialExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Trial/TestTrialEx/TestTrialExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Trial.TestTrialEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTrialExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
